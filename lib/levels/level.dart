2f198f84644571fc3fcb76ec9b502f12838692cb
mohammed hisham <mohamedhis36@gmail.com>
2025-05-11 13:18:11 +0300

    added a level name variable to change the levels

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -5,14 +5,31 @@ import 'package:flame_tiled/flame_tiled.dart';
 import 'package:pixel_adventure/actors/player.dart';
 
 class Level extends World {
+  final String levelName;
+  Level({required this.levelName});
   late TiledComponent level;
 
   @override
   FutureOr<void> onLoad() async {
-    level = await TiledComponent.load('level-01.tmx', Vector2.all(16));
+    //loads the level file that we created
+    level = await TiledComponent.load('$levelName.tmx', Vector2.all(16));
 
     add(level);
-    add(Player());
+    //we get the spawnPoint layer that we created
+    final spawnPointLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+    //we loop through all the spawnPoints and add objects based on the spawnPoint class
+    for (final spawnPoint in spawnPointLayer!.objects) {
+      switch (spawnPoint.class_) {
+        case 'Player':
+          final player = Player(
+            character: 'Mask Dude',
+            position: Vector2(spawnPoint.x, spawnPoint.y),
+          );
+          add(player);
+          break;
+        default:
+      }
+    }
 
     return super.onLoad();
   }
